fd03540218630378bea8eb8e1a71132bf0ae5b9b
bing chilling <jakubsis@seznam.cz>
2025-02-19 13:09:34 +0100

    initial comimit

---
+++ Infastructure/Database/DatabaseContext.cs
@@ -0,0 +1,10 @@
+using Application.Infastructure.Database.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Infastructure.Database;
+
+public class DatabaseContext(DbContextOptions options) : DbContext(options)
+
+{
+    public DbSet<UserDo> Users => Set<UserDo>();
+}
