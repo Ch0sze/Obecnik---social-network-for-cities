b3999ce771b33cd6a00daf0904cab1f32a5a3c30
msedlacek1 <m_sedlacek@utb.cz>
2025-03-16 16:27:32 +0100

    Reapply "create post popup initial"
    This reverts commit 88cc9e3ccf8b710a8eca31e8972234524b72d889.

---
+++ Api/Models/CreatePostViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Api.Models
+{
+    public class PostViewModel
+    {
+        public required string Title { get; set; }
+        public required string Content { get; set; }
+    }
+}
