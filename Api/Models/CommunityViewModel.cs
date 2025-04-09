3eb46bfdc7e5a297d6bb80b960c344852fe4da89
OndSedlacek <o1_sedlacek@utb.cz>
2025-04-09 20:15:19 +0200

    Komplet přestavění renderování obcí kvůli podpoře více komunit

---
+++ Api/Models/CommunityViewModel.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Application.Api.Models
+{
+    public class CommunityViewModel
+    {
+        public string Name { get; set; } = string.Empty;
+        public string? ImageUrl { get; set; } = string.Empty;  // Make ImageUrl nullable
+        public bool Dot { get; set; } = true;
+        public string Link { get; set; } = "/#";
+    }
+}
